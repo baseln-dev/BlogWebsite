2f8919cb9f280cdaafcbb9127617c0e597f9ab11
NullVoyager47 <rufus_reed@hotmail.com>
2026-01-29 16:07:51 +0000

    Updated CNAME and config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://nullvoyager47.github.io/BlogWebsite/',
-	base: '/BlogWebsite/',
+	site: 'https://blog.rreed.dev/',
+	base: '/',
 	output: "static",
 	integrations: [mdx(), sitemap()],
 	build: {
